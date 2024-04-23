2d41e4d507e8add9f74ce9536c0a7872b6ccc107
vinicius <vmm_7@hotmail.com>
2024-04-23 12:38:27 -0300

    Cria a função de empréstimo

--- script.js
+++ script.js
@@ -233,6 +233,23 @@ btnTransfer.addEventListener('click', function (e) {
   }
 });
 
+// EMPRÉSTIMO
+btnLoan.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  const amount = +inputLoanAmount.value;
+
+  if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
+    // adiciona o valor
+    currentAccount.movements.push(amount);
+
+    // atualiza a UI
+    updateUI(currentAccount);
+
+    inputLoanAmount.value = '';
+  }
+});
+
 // DELETA CONTA
 btnClose.addEventListener('click', function (e) {
   e.preventDefault();
